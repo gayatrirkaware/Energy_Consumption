eb513e413708c7e067028e5951ae8cc4e04f657c
gayatrirkaware <gayatrikawarer@gmail.com>
2025-05-05 00:36:36 +0530

    Added configuration to databse

---
+++ project_config.py
@@ -0,0 +1,8 @@
+CSV_FILE_PATH = r"Data\energy_data.csv"
+MODEL_FILE_PATH = r"Artifacts\linear_regression.pkl"
+LABEL_ENC_DATA_PATH = r"Artifacts\label_enc_data.json"
+FLASK_PORT_NUMBER = 8087
+MONGODB_PORT_NUMBER = 27017
+PROJECT_DB_NAME = "energy_consumption_db"
+USER_CRED_COLLECTION_NAME = 'user_details'
+TESTING_COLLECTION_NAME = 'energy_data'
